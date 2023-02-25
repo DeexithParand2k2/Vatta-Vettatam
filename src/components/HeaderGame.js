78b52897cbe2ea8d801ae1abe5a8febad48785f2
DeexithParand2k2 <deexith2002@gmail.com>
2023-02-25 08:40:33 +0530

    updated healthBar UI

--- src/components/HeaderGame.js
+++ src/components/HeaderGame.js
@@ -7,21 +7,21 @@ function HeaderGame({setPlayerState}) {
   return (
     <div className='header' style={{boxSizing:'border-box'}}>
       <div className='headerGame'>
-          <MyHealthBar setPlayerState={setPlayerState}/>
-          <div className='cholasScore'>
+          <MyHealthBar team={1} setPlayerState={setPlayerState}/>
+          {/* <div className='cholasScore'>
             <h3>{Object.keys(setPlayerState.playersTeamOne).length-2}</h3>
-          </div>
+          </div> */}
           <div className='cholasTeamName'>
             <h1>Cholas</h1>
           </div>
-          <img src={vs} height='60px' width='60px'></img>
+          <img src={vs} className='vikingImg' height='60px' width='60px'></img>
           <div className='pandiasTeamName'>
             <h1>Pandias</h1>
           </div>
-          <div className='pandiasScore'>
+          {/* <div className='pandiasScore'>
             <h3>{Object.keys(setPlayerState.playersTeamTwo).length-2}</h3>
-          </div>
-          <MyHealthBar setPlayerState={setPlayerState} />
+          </div> */}
+          <MyHealthBar team={2} setPlayerState={setPlayerState} />
       </div>  
     </div>
   )
--- src/components/MyHealthBar.js
+++ src/components/MyHealthBar.js
@@ -1,35 +1,67 @@
 import React from "react";
 import '../styles/myHealthBar.css'
 
-const HealthBar = ({ maxHp = 100, hp = 100 } = {}) => {
+const HealthBar = ({ team, maxHp = 100, hp = 100 } = {}) => {
   const barWidth = (hp / maxHp) * 100;
-  return (
-    <div>
-      <div className="health-bar">
-        <div className="bar" style={{ width: `${barWidth}%` }}></div>
-        <div className="hit" style={{ width: `${0}%` }}></div>
+  if(team===1){
+    return (
+      <div>
+        <div className="health-bar">
+          <div className="bar" style={{ width: `${barWidth}%`, backgroundColor: "#D60D0A" }}></div>
+          <div className="hit" style={{ width: `${0}%` }}></div>
 
-        <div
-          style={{
-            position: "absolute",
-            top: "5px",
-            left: 0,
-            right: 0,
-            textAlign: "center"
-          }}
-        >
-          {hp} / {maxHp}
+          {/* <div
+            style={{
+              position: "absolute",
+              top: "5px",
+              left: 0,
+              right: 0,
+              textAlign: "center"
+            }}
+          >
+            {hp} / {maxHp}
+          </div> */}
         </div>
-      </div>
 
-      <br />
-    </div>
-  );
+        <br />
+      </div>
+    );
+  }
+  else{
+    return (
+      <div>
+        <div className="health-bar">
+          <div className="bar" style={{ width: `${barWidth}%`, backgroundColor: "#4137CD" }}></div>
+          <div className="hit" style={{ width: `${0}%` }}></div>
+
+          {/* <div
+            style={{
+              position: "absolute",
+              top: "5px",
+              left: 0,
+              right: 0,
+              textAlign: "center"
+            }}
+          >
+            {hp} / {maxHp}
+          </div> */}
+        </div>
+
+        <br />
+      </div>
+    );
+  }
+  
 };
 
-export default function MyHealthBar({setPlayerState}) {
-  return (
-    <HealthBar hp={(Object.keys(setPlayerState.playersTeamOne).length-2)*25} maxHp={100} />  
-  );
+export default function MyHealthBar({team,setPlayerState}) {
+  if(team===1){
+    //setBackGroundColor(team)
+    return <HealthBar team={team} hp={(Object.keys(setPlayerState.playersTeamOne).length-2)*25} maxHp={100} />  
+  }
+  else{
+    //setBackGroundColor(team)
+    return <HealthBar team={team} hp={(Object.keys(setPlayerState.playersTeamTwo).length-2)*25} maxHp={100} />
+  }
 }
 
